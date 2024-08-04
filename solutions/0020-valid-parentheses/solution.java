061fcfaecaa56bec3ad4b79b38399aee11f79418
Sudharsan Kannan <107355692+Sudharsan-K1921@users.noreply.github.com>
2024-08-04 11:07:52 +0000

    [LeetCode Sync] Runtime - 1 ms (99.46%), Memory - 41.5 MB (96.90%)

---
+++ solutions/0020-valid-parentheses/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> ss=new Stack<Character>();
+      for(char c:s.toCharArray()){
+        if(c=='('){
+            ss.push(')');
+
+        }
+        else if(c=='['){
+            ss.push(']');
+        }
+        else if(c=='{'){
+            ss.push('}');
+        }
+        else if(ss.isEmpty() || ss.pop()!=c){
+            return false;
+        }
+      }
+        return ss.isEmpty();
+      
+}
+}
