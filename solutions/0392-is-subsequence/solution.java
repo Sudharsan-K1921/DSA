09e542bfcc131167a02c324a72ce3bf4931177f3
Sudharsan Kannan <107355692+Sudharsan-K1921@users.noreply.github.com>
2024-08-24 09:07:24 +0000

    [LeetCode Sync] Runtime - 1 ms (93.53%), Memory - 41.6 MB (54.38%)

---
+++ solutions/0392-is-subsequence/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+        int j=0;
+        for(int i=0;i<m;i++){
+            // if(!t.contains((Character.toString(s.charAt(i))))){
+            //     return false;
+            // }
+            if(j==n) return true;
+            if(t.charAt(i)==s.charAt(j)){
+                j++;
+            }
+        }
+        return j==n;
+    }
+}
